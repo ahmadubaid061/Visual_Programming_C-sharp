e8675ed6d64dff7b891f8e4be76edb1d5b976908
ahmadubaid061 <ahmadubaidedu@gmail.com>
2026-09-24 00:26:56 +0500

    IDE setup and testing

---
+++ 01lab/project1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace project1
+{
+    internal class Program
+    {
+        const int rows = 10;
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= rows-i; j++)
+                {
+                    Console.Write(" ");
+                }
+                var k = 0;
+                while(k != 2 * i - 1)
+                {
+                    Console.Write("*");
+                    k++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
